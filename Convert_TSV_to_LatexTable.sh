2df9507b7b13f4d8e7faa9ecaf536637c5ce098e
PALLAVI GUPTA <31351096+pallavig702@users.noreply.github.com>
2025-02-12 17:23:46 -0600

    Update Convert_TSV_to_LatexTable.sh

--- Convert_TSV_to_LatexTable.sh
+++ Convert_TSV_to_LatexTable.sh
@@ -1,1 +1,1 @@
-cat Data.tsv | sed 's/^/cite\{/g' | tr '\t' '&' | sed -E 's/^([^\&]*)/\1}/' | sed 's/&$//' | sed -E 's/$/\\\\[0.1em]/' | sed 'G;s/$/\\hline/' > FixedData.Latex.txt
+cat MainTable.tsv | sed 's/^/\cite\{/g' | tr '\t' '&' | sed -E 's/^([^\&]*)/\1}/' | sed 's/&$//' | sed -E 's/$/\\\\[0.1em]/' | sed 'G;s/$/\\hline/' | sed 's/^cite/\\cite/g' > >FixedMainTable.Latex.txt
